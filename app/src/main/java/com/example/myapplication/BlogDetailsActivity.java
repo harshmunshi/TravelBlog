e0127393b0072616f12c7bf27d45b3138cfd80f4
harshmunshi <harsh.munshi@smail.inf.h-brs.de>
2020-10-26 17:25:02 +0100

    added the imageview and activity blog page activity.

---
+++ app/src/main/java/com/example/myapplication/BlogDetailsActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BlogDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_details);
+
+        ImageView imageMain = findViewById(R.id.imageMain);
+        imageMain.setImageResource(R.drawable.sydney_view);
+
+        ImageView imageAvatar = findViewById(R.id.imageAvatar);
+        imageAvatar.setImageResource(R.drawable.avatar);
+
+        TextView textTitle = findViewById(R.id.textTitle);
+        textTitle.setText("G'day from Sydney");
+
+        TextView textDate = findViewById(R.id.textDate);
+        textDate.setText("August 2, 2019");
+
+        TextView textAuthor = findViewById(R.id.textAuthor);
+        textAuthor.setText("Grayson Wells");
+
+        TextView textRating = findViewById(R.id.textRating);
+        textRating.setText("4.4");
+
+        TextView textViews = findViewById(R.id.textViews);
+        textViews.setText("(2687 views)");
+
+        TextView textDescription = findViewById(R.id.textDescription);
+        textDescription.setText("Australia is one of the most popular travel destinations in the world.");
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        ratingBar.setRating(4.4f);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainTextView = findViewById(R.id.maintextView);
+        mainTextView.setText("Hello educative.io");
+
+        // start the new activity
+        startActivity(new Intent(this, BlogDetailsActivity.class));
     }
 }
